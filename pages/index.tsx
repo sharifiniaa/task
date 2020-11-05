a072dea405c545ed91516fd878050f1270811838
pranjal baweja <pranjal.baweja@gmail.com>
2020-11-05 19:44:52 +0400

    feat: inital commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,27 @@
 import { FC } from 'react';
 import { Quotes } from '../src/components/Quotes';
 import { NavBar } from '../src/components/NavBar';
-import { Quote } from '../src/components/types';
+import { QuoteType } from '../src/components/types';
 import { QUOTES } from '../src/data/qutoes';
 import Footer from '../src/components/Footer';
 import QuoteCard from '../src/components/QuoteCard';
 import Pagination from '../src/components/Pagination';
 
 const IndexPage: FC = () => {
-    const quotes: Quote[] = QUOTES;
+    const quotes: QuoteType[] = QUOTES;
     return (
         <>
             <NavBar />
             <div className="container mx-auto  max-w-7xl">
-              
-                <div className="mt-4 flex flex-row">
+                {/* <div className="mt-4 flex flex-row">
                     <div>
                         <p>Search results for 'buddha'</p>
                     </div>
                     <Pagination />
-                </div>
+                </div> */}
                 <div className="flex flex-row flex-wrap justify-start mt-4">
                     {quotes &&
-                        quotes.map((quote: Quote, index: number) => {
+                        quotes.map((quote: QuoteType, index: number) => {
                             return <QuoteCard key={`quote-${index}`} quote={quote} />;
                         })}
                 </div>
